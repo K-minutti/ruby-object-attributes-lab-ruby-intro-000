a5d6c6adbf32ead4d9d1708fc9ac6699ef240ff2
Kevin Minutti <minutti70@gmail.com>
2020-03-19 19:57:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def initialize(name)
+    def initialize(name)
     @name = name
   end
 
@@ -7,4 +7,7 @@ class Dog
     @name
   end
 
+  def name=(new_name)
+    @name = new_name
+  end
 end
