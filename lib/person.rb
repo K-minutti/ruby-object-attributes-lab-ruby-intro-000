6935dfcebd94fd4d9e40ad77e44390dda182885e
Kevin Minutti <minutti70@gmail.com>
2020-03-19 20:03:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,18 @@
+# class Person
+# end
+
 class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
 end
